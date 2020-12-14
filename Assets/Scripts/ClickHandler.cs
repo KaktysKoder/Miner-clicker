c087fdefcac1d79c11b625a83e1aa8c570c2127a
Rimuru Tempest <Kaktyskoder@gmail.com>
2020-12-14 11:05:26 +0400

    Implemented and tested the ability to click on the screen and score points.

---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickHandler : MonoBehaviour
+{
+    [Header("Please set up links to the object.")]
+    [Tooltip("Link to the text of the score counter.")]
+    [SerializeField] private Text ScoreText;
+
+    private int scorCount = default;
+
+    private void Start()
+    {
+        if (ScoreText == null)
+            Debug.LogError("Set a link to the (Score game object) the class Input Handler..");
+    }
+
+    public void OnClick()
+    {
+        scorCount++;
+
+        ScoreText.text = $"{scorCount}$";
+    }
+}
