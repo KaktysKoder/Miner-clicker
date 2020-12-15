a277f47d4a411443734b8c3985895943c9b93a7a
Rimuru Tempest <Kaktyskoder@gmail.com>
2020-12-15 12:21:56 +0400

    Fix bug in shop.

--- Assets/Scripts/ClickHandler.cs
+++ Assets/Scripts/ClickHandler.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class ClickHandler : MonoBehaviour
 {
@@ -17,21 +18,99 @@ public class ClickHandler : MonoBehaviour
     public int[] ShopBonuses;
     [Tooltip("Изменение текста после покупки улучшения.")]
     public Text[] ShopButtonsText;
+    public Button[] ShopButtons;
+
+    public float[] bonusTime;      //Время задержки
 
     private int scorCount = 0;
     private int bonus = 1;
-
+    private int workersCount, workersBonus = 1;
 
     private void Start()
     {
-        if (scoreText == null)
-            Debug.LogError("Set a link to the (Score game object) the class Input Handler..");
+        if (scoreText == null) { Debug.LogError("Set a link to the (Score game object) the class Input Handler.."); }
+
+        StartCoroutine(BounsPerSec());
     }
 
     private void Update()
     {
-        // Обновление отображения счётчика очков.
         scoreText.text = $"{scorCount}$";
+    }
+
+    /// <summary>
+    /// Найм рабочего
+    /// </summary>
+    /// <param name="index"></param>
+    public void HireWorker(int index)
+    {
+        if (scorCount >= ShopCosts[index])
+        {
+            workersCount++;
+
+            scorCount -= ShopCosts[index];
+
+            ShopCosts[index] *= 2;              // Увеличиваем след. стоимость покупки бонуса.
+
+            // Обновляем текст стоимости покупки улучшения.
+            ShopButtonsText[index].text = "Купить рабочего\n" + ShopCosts[index] + "$";
+
+        }
+        else Debug.Log("Не хвотает денег!!");
+    }
+
+    /// <summary>
+    /// Автоклик, рабочие фармят деньги
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator BounsPerSec()
+    {
+        while (true)
+        {
+            scorCount += (workersCount * workersBonus);
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    /// <summary>
+    /// Активация бонуса рабочих
+    /// </summary>
+    /// <param name="index"></param>
+    public void StartBonusTimer(int index)
+    {
+        int cost = 2 * workersCount;
+
+        ShopButtonsText[2].text = "Купить пиво рабочему\n" + ShopCosts[cost] + "$";
+
+        if (scorCount >= cost)
+        {
+            StartCoroutine(BonusTimer(bonusTime[index], index));
+            scorCount -= cost;
+        }
+    }
+
+    /// <summary>
+    /// Бафф - увеличение скорости рабочих на time секунд
+    /// </summary>
+    /// <param name="time">Время работы бонуса</param>
+    /// <param name="index">Индекс кнопки</param>
+    /// <returns></returns>
+    private IEnumerator BonusTimer(float time, int index)
+    {
+        ShopButtons[index].interactable = false;    // Деактивация кликабильности кнопки покупки пива.
+
+        //TODO : Swith
+        if (index == 0 && workersCount > 0)
+        {
+            workersBonus *= 2;
+
+            yield return new WaitForSeconds(time);
+
+            workersBonus /= 2;
+        }
+
+        ShopButtons[index].interactable = true;   // Активация кликабильности кнопки покупки пива.
     }
 
     /// <summary>
@@ -64,12 +143,11 @@ public class ClickHandler : MonoBehaviour
 
             ShopCosts[index] *= 2;              // Увеличиваем след. стоимость покупки бонуса.
 
-                                                // Обновляем текст стоимости покупки улучшения.
+            // Обновляем текст стоимости покупки улучшения.
             ShopButtonsText[index].text = "Купить улучшение\n" + ShopCosts[index] + "$";
         }
         else Debug.Log("Не хвотает денег!!");
     }
-
     #region Test
     public void DoubleScore()
     {
